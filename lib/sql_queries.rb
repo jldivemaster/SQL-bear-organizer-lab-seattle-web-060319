261fb89bed3669fae98d1d473c6a2b2c018ff7cb
Jason Leach <jlspursfan@gmail.com>
2019-06-11 23:56:50 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = 'Y' ORDER BY age ASC;"
+  "SELECT name, age FROM bears WHERE alive = 'Y' ORDER BY age;"
 end
 
 def selects_oldest_bear_and_returns_name_and_age
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT, FROM bears WHERE;"
+  "SELECT color, COUNT FROM bears WHERE;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(temperament), FROM bears WHERE temperament = 'goofy'"
+  "SELECT COUNT(temperament) FROM bears WHERE temperament = 'goofy'"
 end
 
 def selects_bear_that_killed_Tim
