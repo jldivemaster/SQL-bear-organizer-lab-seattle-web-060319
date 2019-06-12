055b8c8c3d7520f091374f2cb9b882c8a62ff2fb
Jason Leach <jlspursfan@gmail.com>
2019-06-12 00:32:48 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, COUNT(*) FROM bears GROUP BY color ORDER BY COUNT(*);"
+  "SELECT color, COUNT(*) FROM bears GROUP BY color ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 # SELECT name, count(name) AS visits
